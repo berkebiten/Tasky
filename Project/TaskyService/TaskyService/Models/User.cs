8b414e2f6492ac70fd34dbf69281e7c0179377e5
Berke Biten <berke.biten@pixelsoftoffice.com>
2021-04-03 20:48:09 +0300

    Initial Commit - Project Creation

---
+++ Project/TaskyService/TaskyService/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace TaskyService.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ProfileImage { get; set; }
+        public bool ActivationStatus { get; set; }
+        public bool Status { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string FirebaseToken { get; set; }
+    }
+}
