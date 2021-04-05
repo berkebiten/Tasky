9469c169febdad0cba821a85506cce4a736302ab
Oğuz Kaan Yazan <oguz.yazan@infoline-tr.com>
2021-04-05 12:01:33 +0300

    project name updated

--- Project/TaskyMobileApp/android/app/src/main/java/com/tasky/MainActivity.java
+++ Project/TaskyMobileApp/android/app/src/main/java/com/tasky/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobileframework;
+package com.tasky;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MobileFramework";
+    return "Tasky";
   }
 }
