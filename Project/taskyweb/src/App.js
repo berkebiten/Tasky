87d85c886a598f896ce7084993c014ef44c4ab8f
Oğuz Kaan Yazan <oguz.yazan@infoline-tr.com>
2021-04-11 17:55:09 +0300

    custom modal updated

--- Project/taskyweb/src/App.js
+++ Project/taskyweb/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SignUp from "./containers/SignUp";
 import SignIn from "./containers/SignIn";
 import Projects from "./containers/Projects";
-import Navbar from "./components/Navbar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/rsuite/dist/styles/rsuite-dark.css';
 import 'semantic-ui-css/semantic.min.css'
+import '../node_modules/antd/dist/antd.css'
 
 
 function App() {
--- Project/taskyweb/src/components/modals/CustomModal.js
+++ Project/taskyweb/src/components/modals/CustomModal.js
@@ -1,19 +1,17 @@
-import {Component} from 'react'
-import { Modal } from "rsuite";
+import { Component } from "react";
+import { Modal } from "antd";
 import { withRouter } from "react-router-dom";
 
 class CustomModal extends Component {
   render() {
     return (
       <Modal
-        show={this.props.isVisible}
-        onHide={this.props.onClose}
-        backdrop={this.props.backdrop}
+        visible={this.props.isVisible}
+        onCancel={this.props.onClose}
+        title={this.props.title}
+        footer={null}
       >
-        <Modal.Header closeButton>
-          <Modal.Title>{this.props.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{this.props.content}</Modal.Body>
+        {this.props.content}
       </Modal>
     );
   }
