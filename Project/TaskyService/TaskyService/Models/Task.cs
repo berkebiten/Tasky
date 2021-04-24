49bc36bb233a79cedbb44946b7c36c2017ad4b6e
Oğuz Kaan Yazan <oguz.yazan@infoline-tr.com>
2021-04-24 17:15:01 +0300

    task controller added

---
+++ Project/TaskyService/TaskyService/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace TaskyService.Models
+{
+    public class Task
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid AssigneeId { get; set; }
+        public Guid ReporterId { get; set; }
+        public Int16 Priority { get; set; }
+        public Int16 Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
