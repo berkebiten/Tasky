51ee20d1daa472075695b908eef17c9a37215876
Berke Biten <berke.biten@pixelsoftoffice.com>
2021-04-25 15:22:14 +0300

    fix

---
+++ Project/TaskyService/TaskyService/Services/MailService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+using TaskyService.DbContexts;
+
+namespace TaskyService.Services
+{
+    public class MailService
+    {
+
+        private readonly MailboxAddress from = new MailboxAddress("Tasky", "taskydev@gmail.com");
+        private readonly string authId = "taskydev@gmail.com";
+        private readonly string authPw = "12122012aA";
+
+        private readonly MailTemplateContext _context;
+
+        public MailService(MailTemplateContext context)
+        {
+            _context = context;
+        }
+
+
+        public string SendMailFromTemplate(string mailTemplateCode, string to, string cc, Hashtable ht)
+        {
+            #region fill email
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress mailTo = new MailboxAddress("User", to);
+
+
+            message.From.Add(from);
+            message.To.Add(mailTo);
+
+            if (!string.IsNullOrEmpty(cc))
+            {
+                MailboxAddress mailCc = new MailboxAddress("Info", cc);
+                message.Cc.Add(mailCc);
+            }
+
+
+            var mailTemplate = _context.MailTemplate.ToList().Where(x => x.Code == mailTemplateCode).FirstOrDefault();
+
+            string mailBody = mailTemplate.Body;
+            string mailSubject = mailTemplate.Subject;
+
+            foreach (DictionaryEntry de in ht)
+            {
+                mailBody = mailBody.Replace(de.Key.ToString(), ((de.Value != null) ? de.Value.ToString() : ""));
+                mailSubject = mailSubject.Replace(de.Key.ToString(), ((de.Value != null) ? de.Value.ToString() : ""));
+            }
+
+
+            message.Body = new TextPart(TextFormat.Html) { Text = mailBody };
+            message.Subject = mailSubject;
+            #endregion
+
+            string response = "";
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate(authId, authPw);
+                client.Send(message);
+                client.Disconnect(true);
+                client.Dispose();
+
+                response = "Activation email sent.";
+
+            }
+            catch (Exception e)
+            {
+                response = "Failed to send email.";
+            }
+            #region client
+
+            #endregion
+
+            return response;
+        }
+    }
+}
