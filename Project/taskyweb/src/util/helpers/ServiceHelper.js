76ce6d815c3a64ac383524016d7482360f42a116
Berke Biten <berke.biten@pixelsoftoffice.com>
2021-06-15 22:41:20 +0300

    Update ServiceHelper.js

--- Project/taskyweb/src/util/helpers/ServiceHelper.js
+++ Project/taskyweb/src/util/helpers/ServiceHelper.js
@@ -3,7 +3,7 @@ import { RootViewHelper } from ".";
 
 export default class ServiceHelper {
   static autoLoginEnabled;
-  static baseUrl = "http://192.168.1.41:5001";
+  static baseUrl = "http://localhost:5001";
   static userId;
   static username;
   static password;
